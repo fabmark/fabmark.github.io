0a7d3201df3e67b829bc29382c8c43e8486cd157
fabmark <111680506+fabmark@users.noreply.github.com>
2022-08-21 10:45:00 +0200

    Test

---
+++ script.js
@@ -0,0 +1,41 @@
+let ABC = " AÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZ.,"
+var titkosABC = " PALIÁBCDEÉFGHÍJKMNOÓÖŐQRSTUÚÜŰVWXYZ.,"
+
+function fade(){
+
+    let txt = document.getElementById('result')
+    txt.classList.add('fade-in')
+    setTimeout(function () {
+        txt.classList.remove("fade-in")
+      }, 3000)
+}
+
+
+function ABCeloallit() {
+    var kulcs = document.getElementById("inptxt").value
+    var ujABC = ABC
+    for (let i = 0; i < kulcs.length; i++) {
+        var ch = kulcs.charAt(i)
+        ujABC = ujABC.replace(ch + "", "")
+    }
+    titkosABC = kulcs + ujABC
+    //console.log(titkosABC)
+    console.log(kulcs)
+}
+
+
+function visszafejt(szoveg) {
+    ABCeloallit()
+    var titkosSzoveg = ""
+    var poz = 0
+    var ch
+    for (let i = 0; i < szoveg.length; i++) {
+        ch = szoveg.charAt(i)
+        poz = titkosABC.indexOf(ch)
+        titkosSzoveg += ABC.charAt(poz)
+    }
+    console.log(titkosSzoveg)
+    
+    document.getElementById("result").innerHTML = titkosSzoveg
+    fade()
+}
